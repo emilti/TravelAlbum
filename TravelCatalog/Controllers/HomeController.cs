909792c866f362192e5a24769bb7c46fec15c68d
emiti <emilti1986@gmail.com>
2017-09-20 00:51:52 +0300

    Test commit for jenkins

--- TravelCatalog/Controllers/HomeController.cs
+++ TravelCatalog/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace TravelCatalog.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "TEST!Your application description page.";
 
             return View();
         }
