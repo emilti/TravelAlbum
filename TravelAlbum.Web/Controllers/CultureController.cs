09391c00369f9145eeb0408b8538fed01553d1fd
emiti <emilti1986@gmail.com>
2017-10-14 20:37:03 +0300

    Changing language option added with culture controller and option in the top page menu

---
+++ TravelAlbum.Web/Controllers/CultureController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace TravelAlbum.Controllers
+{
+    public class CultureController : Controller
+    {
+
+        public RedirectToRouteResult RedirectLanguage(string language, string query)
+        {
+            var urlParts = query.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (urlParts.Length > 1 && (query.Contains("/bg/") || query.Contains("/en/")))
+            {
+                return this.RedirectToRoute(new { lang = language, controller = urlParts[1], action = urlParts[2] });
+            }
+            else if (urlParts.Length > 1 && !(query.Contains("/bg/") || query.Contains("/en/")))
+            {
+                return this.RedirectToRoute(new { lang = language, controller = urlParts[0], action = urlParts[1] });
+            }
+            else
+            {
+                return this.RedirectToRoute(new { lang = language, controller = "Home", action = "Index" });
+            }
+
+        }
+    }
+}
