5241005bcd60ee321522344b538eac7945046df8
emiti <emilti1986@gmail.com>
2017-10-15 11:46:22 +0300

    Redirecting cases for localization amended

--- TravelAlbum.Web/Controllers/CultureController.cs
+++ TravelAlbum.Web/Controllers/CultureController.cs
@@ -10,19 +10,26 @@ namespace TravelAlbum.Controllers
         {
             var urlParts = query.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
 
-            if (urlParts.Length > 1 && (query.Contains("/bg/") || query.Contains("/en/")))
+            if (urlParts.Length == 2 && !(query.Contains("/bg/") || query.Contains("/en/")))
+            {
+                return this.RedirectToRoute(new { lang = language, controller = urlParts[0], action = urlParts[1] });
+            }
+            else if (urlParts.Length == 3 && (query.Contains("/bg/") || query.Contains("/en/")))
             {
                 return this.RedirectToRoute(new { lang = language, controller = urlParts[1], action = urlParts[2] });
             }
-            else if (urlParts.Length > 1 && !(query.Contains("/bg/") || query.Contains("/en/")))
+            else if (urlParts.Length == 3 && !(query.Contains("/bg/") || query.Contains("/en/")))
             {
-                return this.RedirectToRoute(new { lang = language, controller = urlParts[0], action = urlParts[1] });
+                return this.RedirectToRoute(new { lang = language, controller = urlParts[0], action = urlParts[1], id = urlParts[2] });
+            }
+            else if (urlParts.Length == 4 && (query.Contains("/bg/") || query.Contains("/en/")))
+            {
+                return this.RedirectToRoute(new { lang = language, controller = urlParts[1], action = urlParts[2], id = urlParts[3] });
             }
             else
             {
                 return this.RedirectToRoute(new { lang = language, controller = "Home", action = "Index" });
             }
-
         }
     }
 }
