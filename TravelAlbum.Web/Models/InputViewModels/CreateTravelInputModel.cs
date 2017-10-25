0f93ee6a1be5d253690766554d72aaa7fd3a875f
emiti <emilti1986@gmail.com>
2017-10-25 13:19:49 +0300

    Model namespaces changed in account views, add travel view change, create travel input model changed to have required attributes

--- TravelAlbum.Web/Models/InputViewModels/CreateTravelInputModel.cs
+++ TravelAlbum.Web/Models/InputViewModels/CreateTravelInputModel.cs
@@ -8,27 +8,35 @@ namespace TravelAlbum.Web.Models.InputViewModels
 {
     public class CreateTravelInputModel
     {
-        [Display(Name = "BgTitleLabel", ResourceType = typeof(GlobalResources))]
+        [Required]
+        [Display(Name = "BgTitleLabel", ResourceType = typeof(GlobalResources))]        
         public string bgTitle { get; set; }
 
+        [Required]
         [Display(Name = "BgDescriptionLabel", ResourceType = typeof(GlobalResources))]
         public string bgDescription { get; set; }
-        
+
+        [Required]
         [Display(Name = "EnTitleLabel", ResourceType = typeof(GlobalResources))]
         public string enTitle { get; set; }
 
+        [Required]
         [Display(Name = "EnDescriptionLabel", ResourceType = typeof(GlobalResources))]
         public string enDescription { get; set; }
 
+        [Required]
         [Display(Name = "UploadedImage_1", ResourceType = typeof(GlobalResources))]
         public HttpPostedFileBase UploadedImage_1 { get; set; }
 
+        [Required]
         [Display(Name = "UploadedImage_2", ResourceType = typeof(GlobalResources))]
         public HttpPostedFileBase UploadedImage_2 { get; set; }
 
+        [Required]
         [Display(Name = "UploadedImage_3", ResourceType = typeof(GlobalResources))]
         public HttpPostedFileBase UploadedImage_3 { get; set; }
 
+        [Required]
         [Display(Name = "UploadedImage_4", ResourceType = typeof(GlobalResources))]
         public HttpPostedFileBase UploadedImage_4 { get; set; }
 
