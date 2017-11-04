8e33ddcc34f69409a47fa69fbb16207d9ce4cf37
emiti <emilti1986@gmail.com>
2017-11-05 00:37:35 +0200

    Constructor tests added HomeController and TravelsController

---
+++ TravelAlbum.UnitTests/Controllers/HomeControllerTests/Constructor_Should.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using TravelAlbum.DataServices.Contracts;
+using TravelAlbum.Web.Controllers;
+
+namespace TravelAlbum.UnitTests.Controllers.HomeControllerTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void ReturnsAnInstance_WhenParameterIsNotNull()
+        {
+            // Arrange
+            var singleImageServiceMock = new Mock<ISingleImageService>();
+
+            // Act
+            HomeController homeController = new HomeController(singleImageServiceMock.Object);
+
+            // Assert
+            Assert.IsNotNull(homeController);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowException_WhenParameterIsNull()
+        {
+            // Act
+            HomeController homeController = new HomeController(null);
+
+            // Assert
+            Assert.IsNotNull(homeController);
+        }
+    }
+}
---
+++ TravelAlbum.UnitTests/Controllers/TravelsControllerTests/Constructor_Should.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using TravelAlbum.DataServices.Contracts;
+using TravelAlbum.Web.Controllers;
+
+namespace TravelAlbum.UnitTests.Controllers.TravelsControllerTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void ReturnsAnInstance_WhenParametersAreNotNull()
+        {
+            // Arrange
+            var travelServiceMock = new Mock<ITravelService>();
+            var travelTranslationalInfoServiceMock = new Mock<ITravelTranslationalInfoService>();
+            var travelImageServiceMock = new Mock<ITravelImageService>();
+            
+            // Act
+            TravelsController travelsController = new TravelsController(travelServiceMock.Object, travelTranslationalInfoServiceMock.Object, travelImageServiceMock.Object);
+
+            // Assert
+            Assert.IsNotNull(travelsController);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ReturnsAnInstance_WhenParameterTravelImageIsNull()
+        {
+            // Arrange
+            var travelServiceMock = new Mock<ITravelService>();
+            var travelTranslationalInfoServiceMock = new Mock<ITravelTranslationalInfoService>();
+
+            // Act
+            TravelsController travelsController = new TravelsController(travelServiceMock.Object, travelTranslationalInfoServiceMock.Object, null);
+
+            // Assert
+            Assert.IsNotNull(travelsController);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ReturnsAnInstance_WhenParameterTravelTranslationalInfoIsNull()
+        {
+            // Arrange
+            var travelServiceMock = new Mock<ITravelService>();
+            var travelImageServiceMock = new Mock<ITravelImageService>();
+
+            // Act
+            TravelsController travelsController = new TravelsController(travelServiceMock.Object, null, travelImageServiceMock.Object);
+
+            // Assert
+            Assert.IsNotNull(travelsController);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ReturnsAnInstance_WhenParameterTravelServiceIsNull()
+        {
+            // Arrange
+            var travelTranslationalInfoServiceMock = new Mock<ITravelTranslationalInfoService>();
+            var travelImageServiceMock = new Mock<ITravelImageService>();
+
+            // Act
+            TravelsController travelsController = new TravelsController(null, travelTranslationalInfoServiceMock.Object, travelImageServiceMock.Object);
+
+            // Assert
+            Assert.IsNotNull(travelsController);
+        }
+    }
+}
