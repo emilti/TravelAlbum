ad9edf701979b71a29d007aa37ae03a2775e8bb1
emiti <emilti1986@gmail.com>
2017-11-05 21:05:44 +0200

    Ninject changed

--- TravelAlbum.Web/App_Start/NinjectWebCommon.cs
+++ TravelAlbum.Web/App_Start/NinjectWebCommon.cs
@@ -19,14 +19,82 @@ namespace TravelAlbum.Web.App_Start
 
     public static class NinjectWebCommon 
     {
+        //private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+
+        //public static IKernel Kernel
+        //{
+        //    get;
+        //    private set;
+        //}
+
+        //public static Action<IKernel> DependenciesRegistration = kernel =>
+        //{
+        //    // kernel.Bind<ITeleimotDbContext>().To<TeleimotDbContext>();
+        //    // kernel.Bind(typeof(IRepository<>)).To(typeof(GenericRepository<>));
+
+        //    kernel.Bind<TravelAlbumEfDbContext>().ToSelf().InRequestScope();
+        //    kernel.Bind<ITravelAlbumEfDbContextSaveChanges>().To<TravelAlbumEfDbContextSaveChanges>();
+        //    kernel.Bind(typeof(IEfDbSetWrapper<>)).To(typeof(EfDbSetWrapper<>));
+
+        //};
+
+
+        ///// <summary>
+        ///// Starts the application
+        ///// </summary>
+        //public static void Start() 
+        //{
+        //    DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+        //    DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+        //    bootstrapper.Initialize(CreateKernel);
+        //}
+
+        ///// <summary>
+        ///// Stops the application.
+        ///// </summary>
+        //public static void Stop()
+        //{
+        //    bootstrapper.ShutDown();
+        //}
+
+        ///// <summary>
+        ///// Creates the kernel that will manage your application.
+        ///// </summary>
+        ///// <returns>The created kernel.</returns>
+        //private static IKernel CreateKernel()
+        //{
+        //    Kernel = new StandardKernel();
+        //    try
+        //    {
+        //        Kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+        //        Kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+        //        RegisterServices(Kernel);
+        //        return Kernel;
+        //    }
+        //    catch
+        //    {
+        //        Kernel.Dispose();
+        //        throw;
+        //    }
+        //}
+
+        ///// <summary>
+        ///// Load your modules or register your services here!
+        ///// </summary>
+        ///// <param name="kernel">The kernel.</param>
+        //private static void RegisterServices(IKernel kernel)
+        //{
+        //    DependenciesRegistration(kernel);
+        //    kernel.Bind<ITravelService>().To<TravelService>();
+        //    kernel.Bind<ITravelTranslationalInfoService>().To<TravelTranslationalInfoService>();
+        //    kernel.Bind<ITravelImageService>().To<TravelImageService>();
+        //    kernel.Bind<ISingleImageService>().To<SingleImageService>();
+        //    kernel.Bind<ISingleImageTranslationalInfoService>().To<SingleImageTranslationalInfoService>();
+        //}   
+
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
-
-
-        public static IKernel Kernel
-        {
-            get;
-            private set;
-        }
 
         public static Action<IKernel> DependenciesRegistration = kernel =>
         {
@@ -36,20 +104,20 @@ namespace TravelAlbum.Web.App_Start
             kernel.Bind<TravelAlbumEfDbContext>().ToSelf().InRequestScope();
             kernel.Bind<ITravelAlbumEfDbContextSaveChanges>().To<TravelAlbumEfDbContextSaveChanges>();
             kernel.Bind(typeof(IEfDbSetWrapper<>)).To(typeof(EfDbSetWrapper<>));
-            
+
         };
 
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-        
+
         /// <summary>
         /// Stops the application.
         /// </summary>
@@ -57,25 +125,25 @@ namespace TravelAlbum.Web.App_Start
         {
             bootstrapper.ShutDown();
         }
-        
+
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
         /// <returns>The created kernel.</returns>
         private static IKernel CreateKernel()
         {
-            Kernel = new StandardKernel();
+            var kernel = new StandardKernel();
             try
             {
-                Kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
-                Kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
 
-                RegisterServices(Kernel);
-                return Kernel;
+                RegisterServices(kernel);
+                return kernel;
             }
             catch
             {
-                Kernel.Dispose();
+                kernel.Dispose();
                 throw;
             }
         }
@@ -90,8 +158,9 @@ namespace TravelAlbum.Web.App_Start
             kernel.Bind<ITravelService>().To<TravelService>();
             kernel.Bind<ITravelTranslationalInfoService>().To<TravelTranslationalInfoService>();
             kernel.Bind<ITravelImageService>().To<TravelImageService>();
+            kernel.Bind<ITravelImageService>().To<TravelImageService>();
             kernel.Bind<ISingleImageService>().To<SingleImageService>();
             kernel.Bind<ISingleImageTranslationalInfoService>().To<SingleImageTranslationalInfoService>();
-        }        
+        }
     }
 }
