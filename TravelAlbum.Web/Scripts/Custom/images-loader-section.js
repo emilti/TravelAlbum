1311798b46855a8bafc5393e335522003c61780a
emiti <emilti1986@gmail.com>
2018-01-05 17:24:29 +0200

    Travel detailed view styles amended

--- TravelAlbum.Web/Scripts/Custom/images-loader-section.js
+++ TravelAlbum.Web/Scripts/Custom/images-loader-section.js
@@ -13,20 +13,23 @@ function GetData() {
         success: function (data) {
             if (data !== null) {
                 var text = $(".travel-text").html();
-                for (var i = 0; i < data.length; i++) {
-                    $("#single-images-container").append(
-                        "<div class='single-image-container'>" +
-                            "<img class='single-image' src='data:image/jpg;base64," + data[i].ImageData + "' />" +
-                            "<a href='data:image/jpg;base64,{0}'," + data[i].ImageData + ") data-lightbox='image-1'>" +
-                                "<div class='cover-container-left'>" + "</div>" +
-                            "</a>" +
-                            "<a href='Images/Details" + data[i].Id + "'>" +
-                                "<div class='cover-container-right'>" +
-                                "</div>" +
-                            "</a>" +
-                            "<div class='description'>" + data[i].CreatedOn + "</div>" +
-                        "</div>")
+                for (var i = 0; i < data.length; i++) {                   
+                    var imageContainer = "<div class='single-image-container image-in-travel'>" +
+                        "<img class='single-image' src='data:image/jpg;base64," + data[i].ImageData + "' />" +
+                        "<a href='data:image/jpg;base64," + data[i].ImageData + "' data-lightbox='image-1'>" +
+                        "<div class='cover-container-left'>" + "</div>" +
+                        "</a>" +
+                        "<a href='Images/Details" + data[i].Id + "'>" +
+                        "<div class='cover-container-right'>" +
+                        "</div>" +
+                        "</a>" +
+                        "<div class='description'>" + data[i].CreatedOn + "</div>" +
+                        "</div>";
+
+                    text = text.replace("/img" + (i + 1) + "/", imageContainer);
                 }
+
+                $(".travel-text").html(text);
 
             }
         },
